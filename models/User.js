d5c0e1a51306327acb02c8e19f9f0b9e318a644f
damatgre <gregdamato2@gmail.com>
2021-08-10 15:40:56 -0400

    initial User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+//import dependencies
+const { Schema, model, Types } = require('mongoose');
+
+//import dateFormat function
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+        },
+        thoughts: [ 
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
